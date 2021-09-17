e828b498f00019f90ad654937706cd1ea9ba537d
AlanGeeker <alangeeker@outlook.com>
2021-09-18 07:35:11 +0800

    No292_Nim游戏

---
+++ leetcode/src/test/java/No292_Nim游戏.java
@@ -0,0 +1,27 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/nim-game/"></a>
+ */
+@Slf4j
+public class No292_Nim游戏 {
+
+    @Test
+    public void test1() {
+        assert canWinNim(1);
+        assert !canWinNim(4);
+    }
+
+    @Test
+    public void test2() {
+    }
+
+    @Test
+    public void test3() {
+    }
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
