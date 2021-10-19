3937f0e9dea4f3c64958c711f29b8b74fffd4573
AlanGeeker <alangeeker@outlook.com>
2021-10-19 13:25:40 +0800

    No211_添加与搜索单词_数据结构设计 0.2

--- leetcode/src/test/java/No211_添加与搜索单词_数据结构设计.java
+++ leetcode/src/test/java/No211_添加与搜索单词_数据结构设计.java
@@ -1,8 +1,10 @@
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @see <a href="https://leetcode-cn.com/problems/design-add-and-search-words-data-structure/"></a>
@@ -32,20 +34,54 @@ public class No211_添加与搜索单词_数据结构设计 {
 
     class WordDictionary {
 
-        private List<Word> dict = new LinkedList<>();
+        /**
+         * 存储所有单词长度的列表
+         */
+        private List<Integer> wordLengthList = new LinkedList<>();
+
+        /**
+         * 存储所有单词的列表
+         */
+        private Map<Integer, List<Word>> dictMap = new HashMap<>();
 
         public WordDictionary() {
 
         }
 
         public void addWord(String word) {
-            dict.add(new Word(word.length(), word.toCharArray()));
+            int length = word.length();
+
+            // 存储长度
+            if (!wordLengthList.contains(length)) {
+                wordLengthList.add(length);
+            }
+            // 存储长度和单词
+            List<Word> list = dictMap.get(length);
+            if (list == null) {
+                list = new LinkedList<>();
+            }
+            list.add(new Word(word.length(), word.toCharArray()));
+            if (list.size() == 1) {
+                dictMap.put(length, list);
+            }
         }
 
         public boolean search(String word) {
-            for (Word item : dict) {
+            int length = word.length();
+
+            // 是否包含入参的单词长度
+            if (!wordLengthList.contains(length)) {
+                return false;
+            }
+            // 判断入参是否全部为 .
+            if (word.replace(".", "").length() == 0) {
+                return true;
+            }
+
+            List<Word> list = dictMap.get(length);
+            for (Word item : list) {
                 // 比较长度
-                if (item.length == word.length()) {
+                if (item.length == length) {
                     // 比较除 . 之外的字符串
                     // 默认相等
                     boolean flag = true;
