41e8293a4792f0f83e54834154ef7d8cf1354528
AlanGeeker <alangeeker@outlook.com>
2021-12-18 21:37:07 +0800

    LeetCodeWordUtil

--- leetcode/src/test/java/util/LeetCodeWordUtil.java
+++ leetcode/src/test/java/util/LeetCodeWordUtil.java
@@ -18,7 +18,8 @@ public class LeetCodeWordUtil {
 
     @Test
     public void leetCode2dArrFormat() {
-        String leetCodeStr = "[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]";
+//        String leetCodeStr = "[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]";
+        String leetCodeStr = "[[\"X\",\".\",\".\",\"X\"],[\".\",\".\",\".\",\"X\"],[\".\",\".\",\".\",\"X\"]]";
         String javaStr = leetCodeStr;
         javaStr = javaStr.replace("[", "{");
         javaStr = javaStr.replace("],", "},\r\n");
