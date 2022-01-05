8a7fd4d1c3295b0c888ddba2dfddf226256f7da0
AlanGeeker <alangeeker@outlook.com>
2022-01-05 08:12:55 +0800

    add nowcoder

---
+++ nowcoder/src/test/java/Temp.java
@@ -0,0 +1,31 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @see <a href=""></a>
+ */
+@Slf4j
+public class Temp {
+
+    @Test
+    public void test1() {
+    }
+
+    @Test
+    public void test2() {
+    }
+
+    @Test
+    public void test3() {
+    }
+
+    /**
+     * 分析：
+     * 1
+     * 2
+     * 3
+     */
+    public void fun() {
+
+    }
+}
