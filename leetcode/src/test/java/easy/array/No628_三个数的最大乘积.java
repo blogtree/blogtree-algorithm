e048da7ed005f6f72027d62ce27923c731ed9f18
Alan <>
2023-12-06 18:22:40 +0800

    No628_三个数的最大乘积

---
+++ leetcode/src/test/java/easy/array/No628_三个数的最大乘积.java
@@ -0,0 +1,40 @@
+package easy.array;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/maximum-product-of-three-numbers/"></a>
+ */
+@Slf4j
+public class No628_三个数的最大乘积 {
+
+    /**
+     * 标签：数组 排序
+     */
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int length = nums.length;
+        return Math.max(nums[length - 3] * nums[length - 2] * nums[length - 1],
+                nums[length - 1] * nums[0] * nums[1]
+        );
+    }
+
+    @Test
+    public void test1() {
+    }
+
+    @Test
+    public void test2() {
+    }
+
+    @Test
+    public void test3() {
+    }
+
+
+}
+
+
