2a414f31ce3160f3b47505f7b5319ede83dd2fff
Alan <>
2023-12-08 18:07:34 +0800

    No643_子数组最大平均数_1

---
+++ leetcode/src/test/java/easy/array/No643_子数组最大平均数_1.java
@@ -0,0 +1,47 @@
+package easy.array;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @see <a href="https://leetcode.cn/problems/maximum-average-subarray-i/"></a>
+ */
+@Slf4j
+public class No643_子数组最大平均数_1 {
+
+    /**
+     * 标签：数组、滑动窗口
+     */
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < nums.length; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (double) maxSum / k;
+    }
+
+    @Test
+    public void test1() {
+        double maxAverage = findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4);
+        System.out.println(maxAverage);
+    }
+
+    @Test
+    public void test2() {
+        double maxAverage = findMaxAverage(new int[]{5}, 1);
+        System.out.println(maxAverage);
+    }
+
+    @Test
+    public void test3() {
+    }
+
+
+}
+
+
